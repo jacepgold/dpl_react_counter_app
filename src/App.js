4e5e91dee45b1ec64ca5349af38875cd1eff6266
Jace Gold <jacegold@gmail.com>
2018-12-04 10:55:45 -0700

    Export logic to Counter.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from './Counter';
 
 class App extends Component {
-
-  // New way of doing it state
-  state = { value: 0, }
-
-  constructor(props) {
-    super(props);
-    this.dec = this.dec.bind(this);
-  }
-  
-  inc = () => {
-    debugger
-    this.setState({ value: this.state.value + 1, })
-  }
-
-  dec() {
-    debugger
-    this.setState({ value: this.state.value - 1, })
-  }
-
   render() {
     return (
       <div className="App">
         <h1>Counter Example</h1>
-        <p>
-          { this.state.value }
-        </p>
-        <button onClick={ this.dec }>-</button>
-        <button onClick={ this.inc }>+</button>
+        <Counter />
       </div>
     );
   }
--- src/Counter.js
+++ src/Counter.js
@@ -1,2 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 
+class Counter extends React.Component {
+  // New way of doing state
+  state = { value: 0 }
+
+  inc = () => {
+    debugger
+    this.setState({ value: this.state.value + 1, })
+  }
+
+  dec = () => {
+    debugger
+    this.setState({ value: this.state.value - 1, })
+  }
+
+  render() {
+    return(
+      <div>
+        <p>
+          { this.state.value }
+        </p>
+        <button onClick={this.dec}>-</button>
+        <button onClick={this.inc}>+</button>
+      </div>
+    )
+  }
+}
+
+export default Counter;
